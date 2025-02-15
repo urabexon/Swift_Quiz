8afa5a68d114f5b6c0e327f532d0747bec8421c1
urabexon <hiroki708.a48uk@gmail.com>
2025-02-15 09:04:14 +0900

    Initial Commit

---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by 卜部大輝 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarubatsuTests/MarubatsuTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuTests.swift
+//  MarubatsuTests
+//
+//  Created by 卜部大輝 on 2025/02/15.
+//
+
+import Testing
+@testable import Marubatsu
+
+struct MarubatsuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
